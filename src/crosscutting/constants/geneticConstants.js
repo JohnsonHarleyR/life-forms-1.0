a4f7128488d0c116c9b8054060dbf2d75f602f9c
Harley Johnson <hjohnson@uwm.com>
2022-06-15 11:30:17 -0400

    Genetics: Added more color traits. Added method to each constant for checking if that trait is allowed for a creature.

--- src/crosscutting/constants/geneticConstants.js
+++ src/crosscutting/constants/geneticConstants.js
@@ -1,10 +1,14 @@
-import { alterColorByAmount, getRandomIntInRange } from "../logic/universalLogic"
+import { alterColorByAmount,
+    alterColorDarkOrLight,
+    canColorChangeRequirementBeMet,
+    getRandomIntInRange } from "../logic/universalLogic"
 
 
 export const GeneticDefaults = {
     GENERATIONS_TO_BECOME_DOMINANT: 4,
     GENERATIONS_TO_BECOME_PERMANENT: 8,
-    ATTEMPTS_TO_MUTATE_ALLOWED: 15
+    ATTEMPTS_TO_MUTATE_ALLOWED: 15,
+    COLOR_DIFFERENCE_REQUIREMENT: 10
 }
 
 export const Dominance = {
@@ -16,7 +20,14 @@ export const Dominance = {
 export const ColorType = {
     R: "R",
     G: "G",
-    B: "B"
+    B: "B",
+    DARKER: "DARKER",
+    LIGHTER: "LIGHTER"
+}
+
+export const AddOrSubtract = {
+    ADD: "ADD",
+    SUBTRACT: "SUBTRACT"
 }
 
 // genes and accompanying traits
@@ -34,6 +45,9 @@ export const COLOR_DEFAULT = {
     isMutation: false,
     alter: (creature) => {
         return {...creature};
+    },
+    canHaveTrait: () => {
+        return true;
     }
 }
 export const MORE_RED = {
@@ -47,6 +61,34 @@ export const MORE_RED = {
         
         creature.adultColor = newColor;
         creature.color = creature.life.determineColor();
+    },
+    canHaveTrait: (creature) => {
+        let result = canColorChangeRequirementBeMet(
+            creature.adultColor,
+            ColorType.R,
+            AddOrSubtract.ADD);
+        return result;
+    }
+}
+
+export const LESS_RED = {
+    name: "LESS_RED",
+    dominance: Dominance.RECESSIVE,
+    isMutation: true,
+    alter: (creature) => {
+        let originalColor = creature.adultColor;
+        let changeAmount = -1 * getRandomIntInRange(10, 20);
+        let newColor = alterColorByAmount(originalColor, ColorType.R, changeAmount);
+        
+        creature.adultColor = newColor;
+        creature.color = creature.life.determineColor();
+    },
+    canHaveTrait: (creature) => {
+        let result = canColorChangeRequirementBeMet(
+            creature.adultColor,
+            ColorType.R,
+            AddOrSubtract.SUBTRACT);
+        return result;
     }
 }
 
@@ -61,6 +103,118 @@ export const MORE_GREEN = {
         
         creature.adultColor = newColor;
         creature.color = creature.life.determineColor();
+    },
+    canHaveTrait: (creature) => {
+        let result = canColorChangeRequirementBeMet(
+            creature.adultColor,
+            ColorType.G,
+            AddOrSubtract.ADD);
+        return result;
+    }
+}
+
+export const LESS_GREEN = {
+    name: "LESS_GREEN",
+    dominance: Dominance.RECESSIVE,
+    isMutation: true,
+    alter: (creature) => {
+        let originalColor = creature.adultColor;
+        let changeAmount = -1 * getRandomIntInRange(10, 20);
+        let newColor = alterColorByAmount(originalColor, ColorType.G, changeAmount);
+        
+        creature.adultColor = newColor;
+        creature.color = creature.life.determineColor();
+    },
+    canHaveTrait: (creature) => {
+        let result = canColorChangeRequirementBeMet(
+            creature.adultColor,
+            ColorType.G,
+            AddOrSubtract.SUBTRACT);
+        return result;
+    }
+}
+
+export const MORE_BLUE = {
+    name: "MORE_BLUE",
+    dominance: Dominance.RECESSIVE,
+    isMutation: true,
+    alter: (creature) => {
+        let originalColor = creature.adultColor;
+        let changeAmount = getRandomIntInRange(10, 20);
+        let newColor = alterColorByAmount(originalColor, ColorType.B, changeAmount);
+        
+        creature.adultColor = newColor;
+        creature.color = creature.life.determineColor();
+    },
+    canHaveTrait: (creature) => {
+        let result = canColorChangeRequirementBeMet(
+            creature.adultColor,
+            ColorType.B,
+            AddOrSubtract.ADD);
+        return result;
+    }
+}
+
+export const LESS_BLUE = {
+    name: "LESS_BLUE",
+    dominance: Dominance.RECESSIVE,
+    isMutation: true,
+    alter: (creature) => {
+        let originalColor = creature.adultColor;
+        let changeAmount = -1 * getRandomIntInRange(10, 20);
+        let newColor = alterColorByAmount(originalColor, ColorType.B, changeAmount);
+        
+        creature.adultColor = newColor;
+        creature.color = creature.life.determineColor();
+    },
+    canHaveTrait: (creature) => {
+        let result = canColorChangeRequirementBeMet(
+            creature.adultColor,
+            ColorType.B,
+            AddOrSubtract.SUBTRACT);
+        return result;
+    }
+}
+
+export const LIGHTER = {
+    name: "LIGHTER",
+    dominance: Dominance.RECESSIVE,
+    isMutation: true,
+    alter: (creature) => {
+        let originalColor = creature.adultColor;
+        let changeAmount = -1 * getRandomIntInRange(15, 25);
+        let newColor = alterColorDarkOrLight(originalColor, changeAmount);
+        
+        creature.adultColor = newColor;
+        creature.color = creature.life.determineColor();
+    },
+    canHaveTrait: (creature) => {
+        let result = canColorChangeRequirementBeMet(
+            creature.adultColor,
+            ColorType.LIGHTER,
+            AddOrSubtract.ADD);
+        return result;
+    }
+}
+
+export const DARKER = {
+    name: "DARKER",
+    dominance: Dominance.RECESSIVE,
+    isMutation: true,
+    alter: (creature) => {
+        let originalColor = creature.adultColor;
+        let changeAmount = -1 * getRandomIntInRange(15, 25);
+        let newColor = alterColorDarkOrLight(originalColor, changeAmount);
+        
+        creature.adultColor = newColor;
+        creature.color = creature.life.determineColor();
+    },
+    canHaveTrait: (creature) => {
+        let result = canColorChangeRequirementBeMet(
+            creature.adultColor,
+            ColorType.DARKER,
+            AddOrSubtract.SUBTRACT);
+        return result;
     }
 }
 
@@ -68,7 +222,13 @@ export const MORE_GREEN = {
 export const ColorTrait = {
     DEFAULT: COLOR_DEFAULT,
     MORE_RED: MORE_RED,
-    MORE_GREEN: MORE_GREEN
+    LESS_RED: LESS_RED,
+    MORE_GREEN: MORE_GREEN,
+    LESS_GREEN: LESS_GREEN,
+    MORE_BLUE: MORE_BLUE,
+    LESS_BLUE: LESS_BLUE,
+    LIGHTER: LIGHTER,
+    DARKER: DARKER,
 }
 
 // --gene
@@ -76,7 +236,16 @@ export const COLOR_GENE = {
     name: "COLOR_GENE",
     geneType: GeneType.COLOR,
     dominantTraits: [COLOR_DEFAULT],
-    recessiveTraits: [MORE_RED, MORE_GREEN]
+    recessiveTraits: [
+        MORE_RED,
+        LESS_RED,
+        MORE_GREEN,
+        LESS_GREEN,
+        MORE_BLUE,
+        LESS_BLUE,
+        LIGHTER,
+        DARKER
+    ]
 }
 
 
